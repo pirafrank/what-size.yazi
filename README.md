04ba6dceb4e55fb24c2f03875648ab021031b5f7
Francesco Pira <dev@fpira.com>
2024-08-03 21:16:35 +0200

    first actual commit. tested with yazi 0.3.0

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # what-size.yazi
+
+A plugin for [yazi](https://github.com/sxyazi/yazi) to calculate the size of the current selection or the current working directory (if no selection is made).
+
+## Installation
+
+```sh
+ya pack -a 'pirafrank/what-size'
+```
+
+## Usage
+
+Add this to your `~/.config/yazi/keymap.toml`:
+
+```toml
+[manager]
+prepend_keymap = [
+  { on   = [ ".", "s" ], run  = "plugin what-size", desc = "Calc size of selection or cwd" },
+]
+```
+
+Change to whatever keybinding you like.
+
+## License
+
+MIT
---
+++ init.lua
@@ -0,0 +1,64 @@
+
+-- function to get paths of selected elements or current directory
+-- of no elements are selected
+local get_paths = ya.sync(function()
+  local paths = {}
+  -- get selected files
+  for _, u in pairs(cx.active.selected) do
+    paths[#paths + 1] = tostring(u)
+  end
+  -- if no files are selected, get current directory
+  if #paths == 0 then
+    if cx.active.current.cwd then
+      paths[1] = tostring(cx.active.current.cwd)
+    else
+      ya.err("what-size would return nil paths")
+    end
+  end
+  return paths
+end)
+
+-- Function to get total size from du output
+local get_total_size = function(s)
+  local lines = {}
+  for line in s:gmatch("[^\n]+") do lines[#lines + 1] = line end
+  local last_line = lines[#lines]
+  local last_line_parts = {}
+  for part in last_line:gmatch("%S+") do last_line_parts[#last_line_parts + 1] = part end
+  local total_size = last_line_parts[1]
+  return total_size
+end
+
+-- Function to format file size
+local function format_size(size)
+  local units = { "B", "KB", "MB", "GB", "TB" }
+  local unit_index = 1
+
+  while size > 1024 and unit_index < #units do
+    size = size / 1024
+    unit_index = unit_index + 1
+  end
+
+  return string.format("%.2f %s", size, units[unit_index])
+end
+
+return {
+	entry = function()
+    local items = get_paths()
+
+    local cmd = "du"
+    local output, err = Command(cmd):arg("-scb"):args(items):output()
+    if not output then
+      ya.err("Failed to run diff, error: " .. err)
+    else
+      local total_size = get_total_size(output.stdout)
+      formatted_size = format_size(tonumber(total_size))
+      ya.clipboard(formatted_size)
+      ya.notify {
+        title = "What size",
+        content = "Total size: " .. formatted_size .. "\nCopied to clipboard.",
+        timeout = 5,
+      }
+    end
+  end,
+}
